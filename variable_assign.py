8cf8a349c791aa80d34de8abcceeee37b504fe24
pravalika2222 <107530566+pravalika2222@users.noreply.github.com>
2022-06-24 22:04:51 +0530

    Add files via upload

---
+++ variable_assign.py
@@ -0,0 +1,9 @@
+# x,y,z='10,20' # so here the values are less compare to variables 
+# print(x,y,z)  # value error: too many values to unpack(expected 3) xyz:variables ; 10,20=values
+
+# x,y,z='1,2,3,4'  #  so here the variables are less compare to values
+# print(x,y,z)   # value error: too many values to unpack(expected 3) 
+
+# 'a single variables can have multiple values & the datatype of the variables is tuple'
+# n='1,2,3,4,5'
+# print(n,tuple(n)) # 1,2,3,4,5('1', ',' '2' ',' '3' ',' '4' ',' '5')
