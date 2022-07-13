e098fa9c37d2a10c3321dae3651654b8d131ff3f
pravalika2222 <107530566+pravalika2222@users.noreply.github.com>
2022-07-13 21:06:55 +0530

    Update variable_assign.py

--- variable_assign.py
+++ variable_assign.py
@@ -1,8 +1,14 @@
 # x,y,z='10,20' # so here the values are less compare to variables 
 # print(x,y,z)  # value error: too many values to unpack(expected 3) xyz:variables ; 10,20=values
 
+# a,b,c='1,2'  
+# print(a,b)   # value error-too many values to unpack(expected 3)
+
 # x,y,z='1,2,3,4'  #  so here the variables are less compare to values
 # print(x,y,z)   # value error: too many values to unpack(expected 3) 
+
+# a,b='10,20,30,10'
+# print(a,b)      
 
 # 'a single variables can have multiple values & the datatype of the variables is tuple'
 # n='1,2,3,4,5'
